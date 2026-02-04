7ba41eea65aee33affb068ae3a4995a72ed1954b
1Kronos <149811438+1Kronos@users.noreply.github.com>
2026-02-04 22:07:18 +0000

    lhero skellton

---
+++ cb-ui/src/commponents/hero/hero.tsx
@@ -0,0 +1,17 @@
+export default function Hero() {
+
+  return (
+    <>
+      <div className="hero-grid">
+        <h1 className="hero-appname"> Rocklin Institute Of Technology</h1>
+        <p className="hero-account">Account</p>
+        <p className="hero-kanban">Kanban Board</p>
+        <p className="hero-team">Team</p>
+        <p className="hero-friends">Friends</p>
+        <p className="hero-ratings">Ratings</p>
+        <p className="hero-repair">Repair</p>
+        <h2 className="hero-timeline">User Timeline</h2>
+      </div>
+    </>
+  )
+}
--- cb-ui/src/pages/home/home.tsx
+++ cb-ui/src/pages/home/home.tsx
@@ -1,10 +1,17 @@
+import Hero from '../.././commponents/hero/hero'
 import './home.css'
+
 
 export default function Home() {
 
   return (
-    <>
-      
+    <> 
+      <div>
+        <Hero />
+      </div>
+      <div>
+
+      </div>
     </>
   )
 }
